9994cef19026fef26dd5037ef186d40f09d7a4cb
rafaelvicio <rafaelvicio@icloud.com>
2017-12-02 00:59:16 -0200

    Primeiros passos

--- index.js
+++ index.js
@@ -5,3 +5,31 @@ const token = '441362777:AAEP_9a_IUioMQw2mMVJX9KuidQoJ5RtKiw';
 
 // Crie o ot usando pollin, para receber atualizaçoes
 const bot = new TelegramBot(token, {polling: true});
+
+// Printa o objeto mesg enviado para o BOT
+bot.on( 'message', ( msg ) => console.log( 'msg', msg ) )
+
+// Envia uma mensaem quando um novo membro entra no grupo
+bot.on('new_chat_members', (msg) => {
+    bot.sendMessage(msg.chat.id, `Olá ${msg.from.first_name}, bem vindo ao GDG Brasilia!`)
+ })
+
+//bot.onText( /\/echo (.*)/, ( msg, match ) => {
+//  console.log( `echo msg: `, msg ) 
+//  console.log( `echo match: `, match ) 
+//})
+
+// Match e o resultado do teste da RegEx  
+const sendEcho = ( msg, match ) => 
+bot.sendMessage( msg.chat.id, match[ 1 ] )
+    .then( logSuccessEcho( msg, match ) )
+    .catch( logErrorEcho( `Error: ` ) )
+
+bot.onText( /\/echo (.*)/, sendEcho )
+
+// Echo
+const logErrorEcho = ( msg ) => ( err ) => 
+console.log( msg, err )
+
+const logSuccessEcho = ( msg, match ) => ( data ) => 
+console.log( `Success: `, data )
